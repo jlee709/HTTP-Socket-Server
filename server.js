29fb3ee20bee5f8164338dd2da06130401310e7f
Justin Lee <leejwg.3@gmail.com>
2017-10-02 13:59:34 -1000

    initia server code added but not tested. bp taken from node docs

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-console.log('server.js');
+// jshint esversion:6
+console.log('server.js Linked');
+
+
+// goal number 1 is to set up the server first and get it working 
+
+console.log('how to test server here'); 
+
+const net = require('net');
+const http = require('http');
+const PORT = process.env.PORT || 6969;
+const clients = [];
+
+
+
+const server = net.createServer((socket) => {
+  socket.end('goodbye\n');
+}).on('error', (err) => {
+  // handle errors here
+  throw err;
+});
+
+
+
+
+
+server.listen(PORT, () => {
+  console.log(`listening to ${PORT}`);
+});
