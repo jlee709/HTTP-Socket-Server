cdea7069b816894036b3b2b921e77167a4cb6144
Justin Lee <leejwg.3@gmail.com>
2017-10-02 14:02:36 -1000

    client server code added / connection valid on test

--- client.js
+++ client.js
@@ -1,2 +1,25 @@
 // jshint esversion:6
+
 console.log('client linked');
+
+const http = require('http');
+const net = require('net');
+const PORT = process.env.PORT || 6969;
+
+// createing slient connection 
+const client = net.createConnection(PORT, () => {
+  //'connect' listener
+
+  console.log('connected to server!');
+  process.stdin.pipe(client);
+  client.pipe(process.stdout);
+});
+
+
+
+
+
+// writable on port
+process.stdin.on('end', () => {
+  process.stdout.write('end');
+});
