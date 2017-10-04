4c4733cee1200ee24d13bdf0d2914a6df441eb6a
Justin Lee <leejwg.3@gmail.com>
2017-10-03 14:39:59 -1000

    fixed callbacks relatign to read function and uriAcc

--- server.js
+++ server.js
@@ -6,17 +6,18 @@ const fs = require('fs');
 const net = require('net');
 const PORT = process.env.PORT || 8080;
 const path = require('path');
-const uriAcc = findUri(data);
+var uriAcc;
 
 var server = net.createServer((socket) => {
   //call back to fire once data recieved
   socket.on('data', (data) => {
     console.log('recieving data', data.toString());
-    //work on data set here
-    // if(findUri(data){};
-    findUri(data);
-    // call back data end 
+    // thats in 2 callbacks and ends socket after data and call-backs execute
+    uriAcc = findUri(data);
+    read(uriAcc, function(data){
     socket.end();
+    });
+    // call back data end 
   });
   // socket.end('goodbye\n');
 }).on('error', (err) => {
@@ -28,24 +29,48 @@ server.listen(PORT, () => {
   console.log(`listening to ${PORT}`);
 });
 
-  //function to locate and set uri to variable
 
+
+// external functions
+
+//function to locate and set uri to variable
 function findUri(data){
   const header = data.toString();
   const splitingHeader = header.split(" ");
   const seperateHead = header.split(" ");
-  const uri = seperateHead[1];
-  return uri;}
-
-//resading the data
-function read(){
-  
-  fs.readFile(`./html/${findUri}`, (err, data) => {
-  
-  if (!err && data){
-     console.log(data, 'testestesteststsetesetsetststse');
-  } return data;
-  });
+  var uri = seperateHead[1];
+  var uriAcc = uri.toString();
+  console.log(uriAcc, 'this is the URI here right here');
+  return uriAcc;
 }
+
+//reading the data
+function read(filename, cb) {
+  let filepath = path.join(__dirname, '..', 'html', filename);
+  console.log(filepath,' this is the FILEPATH HERE!');
+
+// asynch doesnt fire till future 
+fs.readFile(filepath, (err, data) => {
+  console.log('readFile', data);
+  cb(data);
+});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
