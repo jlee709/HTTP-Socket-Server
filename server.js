fd8a5b34bd8c887e3444f41bcceabe8a846b6584
Justin Lee <leejwg.3@gmail.com>
2017-10-04 15:53:06 -1000

    ade changes to some files

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 // jshint esversion:6
-console.log('server.js Linked');
+
 
 //constant vars
 const fs = require('fs');
 const net = require('net');
 const PORT = process.env.PORT || 8080;
 const path = require('path');
-var uriAcc;
+var headData;
 
-var server = net.createServer((socket) => {
+var server = net.createServer((client) => {
   //call back to fire once data recieved
-  socket.on('data', (data) => {
+  client.on('data', (data) => {
     console.log('recieving data', data.toString());
-    // thats in 2 callbacks and ends socket after data and call-backs execute
-    uriAcc = findUri(data);
-    read(uriAcc, function(data){
+    let contentLength = data.length;
+    let httpResponse = '\n HTTP/1.1 200 OK \n Server: Best Web Server\n Date: ' + 
+    new Date() + '\n Content-Type: text/html, charset=utf-8 \n Content-Length: ' + 
+    contentLength + '\n Connection: keep-alive \n\n ' + data.toString();
+    client.write(httpResponse);
+    
+    // thats in 2 callbacks and ends client after data and call-backs execute
+    headData = processRequest(data);
+    read(headData, function(data){
 
-      socket.end();
+      client.end();
     });
     // call back data end 
   });
-  // socket.end('goodbye\n');
+  // client.end('goodbye\n');
 }).on('error', (err) => {
   // handle errors here
   throw err;
@@ -31,19 +37,19 @@ server.listen(PORT, () => {
   console.log(`listening to ${PORT}`);
 });
 
-
-
 // external functions
 
-//function to locate and set uri to variable
-function findUri(data){
-  const header = data.toString();
-  const splitingHeader = header.split(" ");
-  const seperateHead = header.split(" ");
-  var uri = seperateHead[1];
-  var uriAcc = uri.toString();
-  console.log(uriAcc, 'this is the URI here right here');
-  return uriAcc;
+//function to locate and set uri to variablef
+function processRequest(data){
+  const header = data.toString().split(" ");
+  var method = header[0];
+  var uri = header[1];
+  var cache = header[1];
+  var connection = header[10];
+  console.log(uri, '<-------URI ');
+  console.log(method, "<-------method");
+  console.log(cache, '<-----http');
+  console.log(connection, '<-----connection');
 }
 
 //reading the data
@@ -51,12 +57,12 @@ function read(filename, cb) {
   let filepath = path.join(__dirname, '.', 'html', filename);
   console.log(filepath,' this is the FILEPATH HERE!');
 
-// asynch doesnt fire till future 
-fs.readFile(filepath,'utf8', (err, data) => {
-  if (err) throw err;
-  console.log('readFile', data);
-  cb(data);
-});
+  // asynch doesnt fire till future 
+  fs.readFile(filepath,'utf8', (err, data) => {
+    if (err) throw err;
+    console.log('readFile', data);
+    cb(data);
+  });
 }
 
 
