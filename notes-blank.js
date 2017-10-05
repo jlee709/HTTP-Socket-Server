8dd73c8d77aa84d8c1be847426c1cdb28229cf2c
Justin Lee <leejwg.3@gmail.com>
2017-10-04 16:10:32 -1000

    made req obj

---
+++ notes-blank.js
@@ -0,0 +1,35 @@
+// ThisIsTheBody=body%20
+// recieving data POST /index.html HTTP/1.1
+// cache-control: no-cache
+// Postman-Token: d4bc84ac-65b2-465b-b7f9-11c5eb1b2053
+// ThisHeader: header
+// User-Agent: PostmanRuntime/6.3.2
+// Accept: */*
+// Host: localhost:8080
+// content-type: application/x-www-form-urlencoded
+// accept-encoding: gzip, deflate
+// content-length: 21
+// Connection: keep-alive
+
+// ThisIsTheBody=body%20
+
+
+
+// fs.readFile('/etc/passwd', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// const fs = require('fs');
+// const path = require('path');
+// // read in the file using fs 
+// let filePath = path.join(_dirname, 'test.text');
+// console.log(filePath);
+
+// fs.readFile(filePath,(err, data) => {
+//     console.log();
+// }); 
+// // contets
+
+// let fileContents;  
+//  
